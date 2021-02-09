d347befc9a712248a1c0b5c42160c210d6c6e786
konojunya <junya03dance@gmail.com>
2021-02-09 22:46:32 +0900

    wip

--- src/sum.test.ts
+++
@@ -1,7 +0,0 @@
-import { sum } from "./sum"
-
-describe("sum()", () => {
-  it("sum([1,2]) === 3", () => {
-    expect(sum([1, 2])).toBe(3)
-  })
-})
--- src/sum.ts
+++
@@ -1,3 +0,0 @@
-export const sum = (numbers: Array<number>): number => {
-  return numbers.reduce((pre, cur) => pre + cur, 0)
-}
